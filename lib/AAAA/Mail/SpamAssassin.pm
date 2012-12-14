033b615acc8349be5863153a971fec2b3ef55fac
Steffen Schwigon <schwigon@cpan.org>
2012-12-14 08:28:05 +0000

    Import of SCHWIGON/AAAA-Mail-SpamAssassin-0.002 from CPAN.
    gitpan-cpan-distribution: AAAA-Mail-SpamAssassin
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         SCHWIGON/AAAA-Mail-SpamAssassin-0.002.tar.gz
    gitpan-cpan-author:       SCHWIGON
    gitpan-cpan-maturity:     released

--- lib/AAAA/Mail/SpamAssassin.pm
+++ lib/AAAA/Mail/SpamAssassin.pm
@@ -1,13 +1,13 @@
 use strict;
 use warnings;
 package AAAA::Mail::SpamAssassin;
-# git description: 7632250
+# git description: v0.001-1-g4fcbc88
 
 BEGIN {
   $AAAA::Mail::SpamAssassin::AUTHORITY = 'cpan:SCHWIGON';
 }
 {
-  $AAAA::Mail::SpamAssassin::VERSION = '0.001';
+  $AAAA::Mail::SpamAssassin::VERSION = '0.002';
 }
 # ABSTRACT: making Mail::SpamAssassin installable
 
@@ -35,13 +35,14 @@ For some reason dependency resolution via the CPAN toolchains does not
 work very well for L<Mail::SpamAssassin>. To install it without manual
 work you need dependencies installed beforehand.
 
-C<AAAA::Mail::SpamAssassin> is a L<Task> distribution that makes sure
-that dependencies are installed so that L<Mail::SpamAssassin>
+C<AAAA::Mail::SpamAssassin> is a L<Task>-style distribution that makes
+sure that dependencies are installed so that L<Mail::SpamAssassin>
 installation does not complain.
 
 If you have a dependency on L<Mail::SpamAssassin> add
-C<AAAA::Mail::SpamAssassin> as an additional dependency and the most
-crucial dependencies will be installed before L<Mail::SpamAssassin>.
+C<AAAA::Mail::SpamAssassin> as an additional dependency and (only) the
+most crucial dependencies will be installed before
+L<Mail::SpamAssassin>.
 
 Why the C<'AAAA'>? L<CPAN> and L<CPANPLUS> install prereqs sorted
 alphabetically, the C<'AAAA'> ensures that this prereq is installed
